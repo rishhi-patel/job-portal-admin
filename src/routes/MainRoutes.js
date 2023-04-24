f46c2b1fc12eb0aa9e838f0f6b6b2c372a2f1933
rishi <rishi.patel@dignizant.com>
2023-04-24 17:13:45 +0530

    Update MainRoutes.js

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -24,7 +24,7 @@ const MainRoutes = {
     element: <MainLayout />,
     children: [
         {
-            path: '/',
+            path: '*',
             element: <DashboardDefault />
         },
         {
